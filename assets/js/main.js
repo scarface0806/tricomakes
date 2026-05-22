ce213ffcb073d7c6611e61001e1edc848db6e595
Santhosh <santhoshuxui2023@gmail.com>
2026-05-22 14:36:55 +0530

    update-seo

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,4 @@
-/* ===================================================================
+﻿/* ===================================================================
     
     Author          : Valid Theme
     Template Name   : Antux - Personal Portfolio Template
@@ -78,7 +78,7 @@
 			boxClass: 'wow', // animated element css class (default is wow)
 			animateClass: 'animated', // animation css class (default is animated)
 			offset: 0, // distance to the element when triggering the animation (default is 0)
-			mobile: true, // trigger animations on mobile devices (default is true)
+			mobile: false, // trigger animations on mobile devices (default is true)
 			live: true // act on asynchronously loaded content (default is true)
 		});
 		wow.init();
